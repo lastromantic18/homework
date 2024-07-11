01ee0e472b6880e139a690cde33bc4e9de87dff9
Алексей <blv02495@gmail.com>
2024-07-11 13:25:01 +0300

    Module 2 5

--- module 2 4.py
+++ module 2 4.py
@@ -1,17 +1,6 @@
-numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
-primes = []
-not_primes = []
-
-for number in numbers:
-    is_prime = True
-    for i in range(2, number): 
-        if number % i == 0:
-            is_prime = False
-            break  
-    if is_prime:
-        primes.append(number)
-    else:
-        not_primes.append(number)
-
-print("Простые числа:", primes)
-print("Не простые числа:", not_primes)
+my_tuple = (1,2,3,4,5)
+print(my_tuple[3])
+print(my_tuple[-1])
+print(my_tuple[1:4])
+print(my_tuple[:3])
+print(my_tuple[1:])
